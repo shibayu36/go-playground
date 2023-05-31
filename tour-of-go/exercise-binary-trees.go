390590c95069b0e6089c98bde21b79ddb5fdb8f5
shibayu36 <shibayu36@gmail.com>
2023-05-31 12:37:35 +0900

    Implement Same

--- tour-of-go/exercise-binary-trees.go
+++ tour-of-go/exercise-binary-trees.go
@@ -22,7 +22,30 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return false
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for val1 := range ch1 {
+		val2, ok := <-ch2
+
+		if !ok {
+			return false
+		}
+
+		if val1 != val2 {
+			return false
+		}
+	}
+
+	_, ok := <-ch2
+	if ok {
+		return false
+	} else {
+		return true
+	}
 }
 
 func main() {
@@ -31,4 +54,7 @@ func main() {
 	for val := range ch {
 		println(val)
 	}
+
+	println(Same(tree.New(1), tree.New(1)))
+	println(Same(tree.New(1), tree.New(2)))
 }
