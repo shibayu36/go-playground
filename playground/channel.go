e55ef57c52c5867f2dc870542650703b0fdb5d10
shibayu36 <shibayu36@gmail.com>
2023-05-31 09:38:50 +0900

    channel

---
+++ playground/channel.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	// if using buffered channel, it will not cause deadlock until 2 elements are added
+// 	ch := make(chan int, 2)
+// 	ch <- 1
+// 	ch <- 2
+// 	fmt.Println(<-ch)
+// 	fmt.Println(<-ch)
+// }
+
+func main() {
+	// if make(chan int) is used without goroutine, it will cause deadlock
+	ch := make(chan int)
+	ch <- 1
+	fmt.Println(<-ch)
+}
