002946add1ea5503240f709d64cf6b3755ccaf24
shibayu36 <shibayu36@gmail.com>
2023-06-16 10:51:39 +0900

    Implement Add API

---
+++ diary/calc_test.go
@@ -0,0 +1,51 @@
+package calcapi
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	calc "github.com/shibayu36/go-playground/diary/gen/calc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	logger := log.New(os.Stderr, "LOG: ", log.Lshortfile)
+	service := NewCalc(logger)
+	ctx := context.Background()
+
+	tests := []struct {
+		name   string
+		inputA int
+		inputB int
+		want   int
+	}{
+		{
+			name:   "Add positive numbers",
+			inputA: 5,
+			inputB: 3,
+			want:   8,
+		},
+		{
+			name:   "Add negative numbers",
+			inputA: -2,
+			inputB: -3,
+			want:   -5,
+		},
+		{
+			name:   "Add zero",
+			inputA: 0,
+			inputB: 0,
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := service.Add(ctx, &calc.AddPayload{A: tt.inputA, B: tt.inputB})
+			assert.Nil(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
--- diary/go.mod
+++ diary/go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dimfeld/httppath v0.0.0-20170720192232-ee938bf73598 // indirect
 	github.com/dimfeld/httptreemux/v5 v5.5.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -17,8 +18,10 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/manveru/faker v0.0.0-20171103152722-9fbc68a78c4d // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/smartystreets/assertions v1.13.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/zach-klippenstein/goregen v0.0.0-20160303162051-795b5e3961ea // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
