18c36bd0bd245eaa8ff2f2b65d9d8642b8d37c7a
shibayu36 <shibayu36@gmail.com>
2023-06-23 14:56:58 +0900

    throttling sample

---
+++ playground/throttling_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func downloadJSON(u string) {
+	println(u)
+	time.Sleep(time.Second * 1)
+}
+
+func TestThrottling(t *testing.T) {
+	before := time.Now()
+
+	limit := make(chan struct{}, 20)
+	var wg sync.WaitGroup
+	for i := 1; i <= 100; i++ {
+		wg.Add(1)
+
+		i := i
+		go func() {
+			// Block if limit channel is over 20
+			limit <- struct{}{}
+			defer wg.Done()
+
+			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
+			downloadJSON(u)
+			<-limit
+		}()
+	}
+	wg.Wait()
+
+	fmt.Printf("%v\n", time.Since(before))
+}
