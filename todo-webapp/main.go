092107af3f805fadbd8f9c5067c90acd20187ad8
shibayu36 <shibayu36@gmail.com>
2023-06-28 09:41:53 +0900

    Create todos table and initialization code

--- todo-webapp/main.go
+++ todo-webapp/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"log"
 	"net/http"
 
+	_ "github.com/lib/pq"
+
 	"github.com/labstack/echo/v4"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 func main() {
+	sqldb, err := sql.Open("postgres", "postgres://username:password@localhost:5432/todo?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqldb.Close()
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+	defer db.Close()
+
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.FromEnv("BUNDEBUG"),
+	))
+
+	ctx := context.Background()
+	_, err = db.NewCreateTable().Model((*Todo)(nil)).IfNotExists().Exec(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
