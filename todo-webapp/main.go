006f4ca84a4fce54e2963bd537f0a00e61d3af97
shibayu36 <shibayu36@gmail.com>
2023-06-28 16:39:25 +0900

    Implement controllers

--- todo-webapp/main.go
+++ todo-webapp/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/lib/pq"
 
@@ -35,10 +38,119 @@ func main() {
 	}
 
 	e := echo.New()
-	e.GET("/", index)
+
+	h := &handlers{e: e, db: db}
+
+	e.GET("/", h.index)
 	e.Logger.Fatal(e.Start(":8989"))
 }
 
-func index(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+type Data struct {
+	Todos  []Todo
+	Errors []error
+}
+
+type handlers struct {
+	e  *echo.Echo
+	db *bun.DB
+}
+
+func (h *handlers) index(c echo.Context) error {
+	var todos []Todo
+	ctx := context.Background()
+	err := h.db.NewSelect().Model(&todos).Order("created_at").Scan(ctx)
+	if err != nil {
+		h.e.Logger.Error(err)
+		return c.Render(http.StatusBadRequest, "index", Data{
+			Errors: []error{errors.New("Cannot get todos")},
+		})
+	}
+
+	return c.Render(http.StatusOK, "index", Data{Todos: todos})
+}
+
+func (h *handlers) indexPost(c echo.Context) error {
+	var todo Todo
+
+	errs := echo.FormFieldBinder(c).
+		Int64("id", &todo.ID).
+		String("content", &todo.Content).
+		Bool("done", &todo.Done).
+		CustomFunc("until", customFunc(&todo)).
+		BindErrors()
+
+	if errs != nil {
+		h.e.Logger.Error(errs)
+		return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+	} else if todo.ID == 0 {
+		// IDが0の時は登録
+		ctx := context.Background()
+		if todo.Content == "" {
+			return renderBadRequest(c, "Content is empty")
+		} else {
+			_, err := h.db.NewInsert().Model(&todo).Exec(ctx)
+			if err != nil {
+				h.e.Logger.Error(err)
+				return renderBadRequest(c, err)
+			}
+		}
+	} else {
+		ctx := context.Background()
+		if c.FormValue("delete") != "" {
+			// 削除
+			_, err := h.db.NewDelete().Model(&todo).Where("id = ?", todo.ID).Exec(ctx)
+			if err != nil {
+				h.e.Logger.Error(err)
+				return renderBadRequest(c, err)
+			}
+		} else {
+			// 更新
+			var orig Todo
+			err := h.db.NewSelect().Model(&orig).Where("id = ?", todo.ID).Scan(ctx)
+			if err != nil {
+				h.e.Logger.Error(err)
+				return renderBadRequest(c, err)
+			}
+
+			orig.Done = todo.Done
+			_, err = h.db.NewUpdate().Model(&orig).Where("id = ?", todo.ID).Exec(ctx)
+			if err != nil {
+				h.e.Logger.Error(err)
+				return renderBadRequest(c, err)
+			}
+		}
+	}
+
+	return c.Redirect(http.StatusFound, "/")
+}
+
+func customFunc(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to decode time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
+}
+
+func renderBadRequest(c echo.Context, errs any) error {
+	var retErrors []error
+
+	switch e := errs.(type) {
+	case []error:
+		retErrors = e
+	case error:
+		retErrors = []error{e}
+	case string:
+		retErrors = []error{errors.New(e)}
+	default:
+		return fmt.Errorf("Unsupported type: %T", errs)
+	}
+
+	return c.Render(http.StatusBadRequest, "index", Data{Errors: retErrors})
 }
--- todo-webapp/todo.go
+++ todo-webapp/todo.go
@@ -9,10 +9,10 @@ import (
 type Todo struct {
 	bun.BaseModel `bun:"table:todos,alias:t"`
 
-	ID        int64     `bun:"id,pk,autoincrement"`
-	Content   string    `bun:"content,notnull"`
-	Done      bool      `bun:"done"`
-	Until     time.Time `bun:"until,nullzero"`
+	ID        int64     `bun:"id,pk,autoincrement" form:"id"`
+	Content   string    `bun:"content,notnull" form:"content"`
+	Done      bool      `bun:"done" form:"done"`
+	Until     time.Time `bun:"until,nullzero" form:"until"`
 	CreatedAt time.Time
 	UpdatedAt time.Time `bun:",nullzero"`
 	DeletedAt time.Time `bun:",soft_delete,nullzero"`
