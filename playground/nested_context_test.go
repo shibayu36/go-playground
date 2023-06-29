b43770d1a25caf9388446edabe296fe66ba37b56
shibayu36 <shibayu36@gmail.com>
2023-06-29 11:18:08 +0900

    nested context

---
+++ playground/nested_context_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestNestedContext(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Create a child context
+	childCtx, childCancel := context.WithCancel(ctx)
+	defer childCancel()
+
+	go func() {
+		time.Sleep(time.Second * 2)
+		cancel()
+	}()
+
+	go func() {
+		time.Sleep(time.Second * 3)
+		// time.Sleep(time.Second * 1)
+		childCancel()
+	}()
+
+	parentCanceled, childCanceled := false, false
+
+	for {
+		if parentCanceled && childCanceled {
+			t.Log("Both contexts are canceled")
+			break
+		}
+
+		select {
+		case <-childCtx.Done():
+			if !childCanceled {
+				t.Log("Child context is done")
+				childCanceled = true
+			}
+		case <-ctx.Done():
+			if !parentCanceled {
+				t.Log("Parent context is done")
+				parentCanceled = true
+			}
+		}
+	}
+}
