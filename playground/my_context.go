525403ed5c4c71956e0449ee6d29b69f58075ff3
shibayu36 <shibayu36@gmail.com>
2025-02-02 16:02:09 +0900

    一旦親のcontextのcancelが伝播するように

--- playground/my_context.go
+++ playground/my_context.go
@@ -1,24 +1,39 @@
 package main
 
+import (
+	"sync"
+)
+
 type MyContext interface {
 	Done() <-chan struct{}
 }
 
 type MyContextBackground struct{}
 
+func NewMyContextBackground() *MyContextBackground {
+	return &MyContextBackground{}
+}
+
 func (c *MyContextBackground) Done() <-chan struct{} {
 	return nil
 }
 
 type MyContextWithCancel struct {
-	done chan struct{}
+	done     chan struct{}
+	doneOnce sync.Once
 }
 
-func NewMyContextWithCancel() (*MyContextWithCancel, func()) {
+func NewMyContextWithCancel(parent MyContext) (*MyContextWithCancel, func()) {
+	if parent == nil {
+		panic("cannot create context from nil parent")
+	}
+
 	done := make(chan struct{})
 	ctx := &MyContextWithCancel{
 		done: done,
 	}
+	ctx.propagateCancel(parent)
+
 	return ctx, ctx.cancel
 }
 
@@ -27,5 +42,23 @@ func (c *MyContextWithCancel) Done() <-chan struct{} {
 }
 
 func (c *MyContextWithCancel) cancel() {
-	close(c.done)
+	c.doneOnce.Do(func() {
+		close(c.done)
+	})
+}
+
+func (c *MyContextWithCancel) propagateCancel(parent MyContext) {
+	parentDone := parent.Done()
+	if parentDone == nil {
+		return
+	}
+
+	go func() {
+		select {
+		case <-parentDone:
+			c.cancel()
+		case <-c.Done():
+			// 自分がキャンセルされたら親のDone待ちを抜ける
+		}
+	}()
 }
--- playground/my_context_test.go
+++ playground/my_context_test.go
@@ -6,16 +6,52 @@ import (
 )
 
 func Test_MyContext(t *testing.T) {
-	ctx, cancel := NewMyContextWithCancel()
-	go func() {
-		time.Sleep(500 * time.Millisecond)
-		cancel()
-	}()
 
-	select {
-	case <-ctx.Done():
-		// 一定時間後にctx.Done()を抜けられる
-	case <-time.After(1 * time.Second):
-		t.Fatal("Timeout")
-	}
+	t.Run("500ms後にcancel", func(t *testing.T) {
+		ctx, cancel := NewMyContextWithCancel(NewMyContextBackground())
+		go func() {
+			time.Sleep(500 * time.Millisecond)
+			cancel()
+		}()
+
+		select {
+		case <-ctx.Done():
+			// 一定時間後にctx.Done()を抜けられる
+		case <-time.After(1 * time.Second):
+			t.Fatal("Timeout")
+		}
+	})
+
+	t.Run("親がcancelされたら子もcancelされる", func(t *testing.T) {
+		parent, cancel := NewMyContextWithCancel(NewMyContextBackground())
+		child1, _ := NewMyContextWithCancel(parent)
+		child2, _ := NewMyContextWithCancel(parent)
+
+		go func() {
+			time.Sleep(100 * time.Millisecond)
+			cancel()
+		}()
+
+		// 全てのコンテキストのDone()チャネルがcloseされることを確認
+		select {
+		case <-parent.Done():
+			// parentがキャンセルされたことを確認
+		case <-time.After(1 * time.Second):
+			t.Fatal("parent context was not cancelled")
+		}
+
+		select {
+		case <-child1.Done():
+			// child1がキャンセルされたことを確認
+		case <-time.After(100 * time.Millisecond):
+			t.Fatal("child1 context was not cancelled")
+		}
+
+		select {
+		case <-child2.Done():
+			// child2がキャンセルされたことを確認
+		case <-time.After(100 * time.Millisecond):
+			t.Fatal("child2 context was not cancelled")
+		}
+	})
 }
